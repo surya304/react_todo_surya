82a41b00281871148fd57fa681d7e4d4d126b5d6
Surya <ksaisurya304@gmail.com>
2023-09-11 17:32:06 -0400

    changed setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function App() {
@@ -48,11 +49,13 @@ export default function App() {
     }))
 
 
-
+ 
 
 
 
   }
+
+  
 
   function deletelist(id) {
 
