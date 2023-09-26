5976edaf5ba3b116b01d14c899d55b1bcdf57429
Surya <ksaisurya304@gmail.com>
2023-09-26 16:59:25 -0400

    learned new stuff about dynamic binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,214 +1,74 @@
-import { useState, useEffect } from "react"
-import { v4 as uuidv4 } from 'uuid';
-import { useNavigate } from 'react-router-dom';
-
+const links = [
+  { name: 'Open roles', href: '#' },
+  { name: 'Internship program', href: '#' },
+  { name: 'Our values', href: '#' },
+  { name: 'Meet our leadership', href: '#' },
+]
+const stats = [
+  { name: 'Offices worldwide', value: '12' },
+  { name: 'Full-time colleagues', value: '300+' },
+  { name: 'Hours per week', value: '40' },
+  { name: 'Paid time off', value: 'Unlimited' },
+]
 
 export default function App() {
-
-  const [currentInput, setcurrentInput] = useState("")
-  const [currenEditedtInput, setcurrenEditedtInput] = useState("")
-
-  const [todoList, settodoList] = useState(() => {
-
-    const localValue = localStorage.getItem("todolist");
-
-    if (localValue == null) return [];
-
-
-
-
-    var parsedJSON = JSON.parse(localValue);
-
-    return parsedJSON.map(todo => {
-      if (todo.is_edit == true) {
-        return { ...todo, is_edit: false }
-      }
-
-      return todo
-    })
-
-
-
-  })
-
-
-  useEffect(() => {
-    localStorage.setItem("todolist", JSON.stringify(todoList))
-  }, [todoList])
-
-
-
-  function submitdata() {
-
-
-    settodoList((currentdata => {
-      return [
-        ...currentdata,
-        { 'id': uuidv4(), 'name': currentInput, 'current': true, is_edit: false }
-      ]
-    }))
-
-
- 
-
-
-
-  }
-
-  
-
-  function deletelist(id) {
-
-
-
-    settodoList((currentInput => {
-
-      return currentInput.filter(item => item.id != id);
-    }))
-
-
-
-
-  }
-
-
-  function Editdata(id) {
-
-    var filtereddata = todoList.filter(item => item.id == id)[0];
-
-    // console.log(filtereddata, "filtered")
-
-    settodoList(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, is_edit: true }
-        }
-
-        return todo
-      })
-    })
-
-
-
-
-    setcurrenEditedtInput(filtereddata.name)
-
-  }
-
-  // function editedtyped(data, id) {
-
-  //   console.log(data,"data")
-
-
-  //   settodoList((item => {
-
-  //     return item.map(item1 => {
-  //       if (item1.id === id) {
-  //         return { ...item1, name: data }
-  //       }
-  //     })
-  //   }))
-
-  // }
-
-
-  function saveEditedData(id) {
-
-
-    settodoList((item => {
-
-      return item.map(i1 => {
-
-        if (i1.id === id) {
-          return { ...i1, name: currenEditedtInput ,is_edit:false }
-        }
-
-        return i1
-
-      })
-    }))
-
-  }
-
   return (
-    <>
-
-
-      <div className="w-60">
-
-
-        <input
-          type="text"
-          value={currentInput}
-          onChange={e => setcurrentInput(e.target.value)}
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
-
-
-      </div>
-
-
-      <button
-
-        onClick={submitdata}
-        type="button"
-        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <img
+        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+      <div
+        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
       >
-        Button text
-      </button>
-
-
-
-
-      <div>
-
-
-        {todoList.map((item) => (
-          <div
-            key={item.id}
-          >
-
-            {item.is_edit == false && <a>
-              {item.name}
-            </a>}
-
-
-            {/* {item.is_edit == true && <input type="text" value={item.name} onChange={e => editedtyped(e.target.value, item.id)} placeholder="Please Edite here" />} */}
-            {item.is_edit == true && <input type="text" value={currenEditedtInput} onChange={e => setcurrenEditedtInput(e.target.value)} placeholder="Please Edite here" />}
-
-
-            {item.is_edit == true && <button
-
-              onClick={() => saveEditedData(item.id)}
-              type="button"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Save EDited Data
-            </button>
-            }
-{item.is_edit == false && 
-            <button
-
-              onClick={() => Editdata(item.id)}
-              type="button"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              EDit
-            </button>
-
-}
-
-            <button
-              onClick={() => deletelist(item.id)}
-
-              style={{ 'border': '1px solid red' }}> Delete  </button>
-          </div>
-        ))}
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
       </div>
-
-
-    </>
+      <div
+        className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+            fugiat veniam occaecat fugiat aliqua.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+            {links.map((link) => (
+              <a key={link.name} href={link.href}>
+                {link.name} <span aria-hidden="true">&rarr;</span>
+              </a>
+            ))}
+          </div>
+          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+            {stats.map((stat) => (
+              <div key={stat.name} className="flex flex-col-reverse">
+                <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>
+                <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
   )
 }
-
-
